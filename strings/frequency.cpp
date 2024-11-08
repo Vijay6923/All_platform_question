0ff4fa05daefdab8d6a6ae73cc4bd8133871f879
Vijay6923 <kumarviju2468@gmail.com>
2024-11-08 20:57:15 +0530

    comment

--- strings/frequency.cpp
+++ strings/frequency.cpp
@@ -16,7 +16,8 @@ int main(){
             max=arr[i];
         }
     }
-    for(int i=0;i<26;i++){
+    // comments
+    for (int i=0;i<26;i++){
         if(arr[i]==max){
             int ascii=i+97;
             char ch= char(ascii);
