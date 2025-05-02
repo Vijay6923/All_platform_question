f4f1752a630f661d0fcab9d3c876fd71d986b5ce
Vijay6923 <kumarviju2468@gmail.com>
2025-05-02 20:39:01 +0530

    cp sheet

--- SPOJ/divisor_summaton.cpp
+++ SPOJ/divisor_summaton.cpp
@@ -1,20 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const ll MOD = 1e9 + 7;
 int main(){
-    long long n;
-    cin>>n;
-    long long ans=0;
-    for(long long l=1,r;l<n;l=r+1){
-        long long q=n/l;
-         r=n/q;
-        long long count=r-l+1;
-        ll sum = ((l + r) % MOD) * (count % MOD) % MOD;
-        sum = (sum * ((MOD + 1) / 2)) % MOD;
-        ll total = (q % MOD) * sum % MOD;
-        ans = (ans + total) % MOD;
-        ans=ans-n;
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        if(n==1){
+            cout<<0<<endl;
+            continue;
+        }
+        ll sum=1;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                sum+=i;
+                if(i!=n/i && n/i!=n){
+                    sum+=n/i;
+                }
+            }
+        }
+        cout<<sum<<endl;
     }
-    cout<<ans<<endl;
+    
 }
