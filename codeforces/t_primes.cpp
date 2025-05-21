904e15906b93f1daaca7a11d337a4327111ba629
Vijay6923 <kumarviju2468@gmail.com>
2025-05-21 19:55:44 +0530

    codeforces

---
+++ codeforces/t_primes.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+bool isPrime(ll n) {
+    if(n <= 1) return false;
+    for(ll i = 2; i*i <= n; i++) {
+        if(n % i == 0) return false;
+    }
+    return true;
+}
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> arr(n);
+    for(ll i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for(ll i = 0; i < n; i++) {
+        ll sq = sqrt(arr[i]);
+        if(sq * sq == arr[i] && isPrime(sq)) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+}
