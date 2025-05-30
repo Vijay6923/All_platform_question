c8a01ac818ab0b914c8dcda889af891bff3c90b1
Vijay6923 <kumarviju2468@gmail.com>
2025-05-30 23:32:36 +0530

    correct answer

--- codeforces/900/from_hero_to_zero.cpp
+++ codeforces/900/from_hero_to_zero.cpp
@@ -1,26 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int main(){
+int main() {
     ll t;
-    cin>>t;
-    while(t--){
-        ll n,k;
-        cin>>n>>k;
-        ll ans=0;
-        while(n!=0){
-            if(n%k==0){
-                ans++;
-                n/=k;
-            }
-            else{
-                n--;
-                ans++;
+    cin >> t;
+    while (t--) {
+        ll n, k;
+        cin >> n >> k;
+        ll ans = 0;
+        while (n != 0) {
+            if (n % k == 0) {
+                ans++;     
+                n /= k;
+            } else {
+                ll rem = n % k;
+                ans += rem;
+                n -= rem;
             }
         }
-        cout<<ans<<endl;
-        
+        cout << ans << endl;
     }
     
 }
