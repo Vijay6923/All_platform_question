9a7659c62ac642f9dc279bd39601f5cc6ce54196
Vijay6923 <kumarviju2468@gmail.com>
2025-07-18 20:33:12 +0530

    prefix sum

---
+++ leetcode/subarray_sum.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> diff(n + 1, 0);
+
+        for (int i = 0; i < n; ++i) {
+            int start = max(0, i - nums[i]);
+            diff[start]++;
+            diff[i + 1]--;
+        }
+
+        for (int i = 1; i < n; ++i) {
+            diff[i] += diff[i - 1];
+        }
+
+        int subSum = 0;
+        for (int i = 0; i < n; ++i) {
+            subSum += nums[i] * diff[i];
+        }
+
+        return subSum;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 2, 1};
+    cout << sol.subarraySum(nums) << endl;
+    return 0;
+}
