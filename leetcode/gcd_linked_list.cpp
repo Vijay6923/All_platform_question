796a3caf57c0241c3a9a3b16d0295998b93103ef
Vijay6923 <kumarviju2468@gmail.com>
2025-07-23 20:57:16 +0530

    updated code

--- leetcode/gcd_linked_list.cpp
+++ leetcode/gcd_linked_list.cpp
@@ -1,28 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// Definition of a singly-linked list node
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode(int x, ListNode *nextNode) : val(x), next(nextNode) {}
 };
 
+// Class with the required logic
 class Solution {
 public:
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
-        if (head == nullptr || head->next == nullptr) return head;
+        if (!head || !head->next) return head;
 
-        ListNode* temp = head;
-        while (temp->next != nullptr) {
-            int g = __gcd(temp->val, temp->next->val);
-            ListNode* node = new ListNode(g, temp->next);
-            temp->next = node;
-            temp = node->next;
+        ListNode* current = head;
+        while (current->next) {
+            int gcdValue = __gcd(current->val, current->next->val);
+            ListNode* gcdNode = new ListNode(gcdValue, current->next);
+            current->next = gcdNode;
+            current = gcdNode->next;
         }
+
         return head;
     }
 };
+
+// Helper to print the linked list
 void printList(ListNode* head) {
     while (head) {
         cout << head->val;
@@ -32,24 +37,30 @@ void printList(ListNode* head) {
     cout << endl;
 }
 
-ListNode* createList(const vector<int>& vals) {
-    if (vals.empty()) return nullptr;
-    ListNode* head = new ListNode(vals[0]);
-    ListNode* curr = head;
-    for (int i = 1; i < vals.size(); i++) {
-        curr->next = new ListNode(vals[i]);
-        curr = curr->next;
+// Helper to create a linked list from vector
+ListNode* createList(const vector<int>& values) {
+    if (values.empty()) return nullptr;
+
+    ListNode* head = new ListNode(values[0]);
+    ListNode* tail = head;
+
+    for (size_t i = 1; i < values.size(); ++i) {
+        tail->next = new ListNode(values[i]);
+        tail = tail->next;
     }
+
     return head;
 }
 
+// Main function to test the logic
 int main() {
-    vector<int> vals = {18, 6, 10, 3};
-    ListNode* head = createList(vals);
-    
+    vector<int> input = {18, 6, 10, 3};
+    ListNode* head = createList(input);
+
     Solution sol;
-    ListNode* modifiedHead = sol.insertGreatestCommonDivisors(head);
-    
-    printList(modifiedHead);  
+    ListNode* updatedHead = sol.insertGreatestCommonDivisors(head);
+
+    printList(updatedHead);  // Output: 18 -> 6 -> 6 -> 2 -> 10 -> 1 -> 3
+
     return 0;
 }
