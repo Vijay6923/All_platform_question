b9014fb659e2a790b5fe3b59e07a46aca63efa4c
Vijay6923 <kumarviju2468@gmail.com>
2025-08-05 20:02:20 +0530

    problem of the day

---
+++ leetcode/fruits_basket.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+    int n=fruits.size();
+    int ans=n;
+    for(int i=0;i<n;i++){
+        for(int j=0; j<n; j++){
+            if (fruits[i]<=baskets[j]){
+                ans--;
+                baskets[j]=0;
+                break;
+            }
+        }
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>fruits(n),baskets(n);
+    for(int i=0;i<n;i++){
+        cin>>fruits[i];
+    }
+    for(int i=0;i<n;i++){
+        cin>>baskets[i];
+    }
+    cout<<numOfUnplacedFruits(fruits,baskets)<<endl;
+}
