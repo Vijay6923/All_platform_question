79ebf334abbc6413284e66b4c8f314db8e450d25
Vijay6923 <kumarviju2468@gmail.com>
2025-08-21 20:32:53 +0530

    contest

---
+++ codeforces/homewok.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string a;
+        cin>>a;
+        int m;
+        cin>>m;
+        string b,c;
+        cin>>b>>c;
+        for(int i = 0;i<m;++i){
+            if(c[i] =='V'){
+                a = b[i] + a;
+            } 
+            else 
+            {
+                a.push_back(b[i]);
+            }
+            
+        }
+        
+        
+        cout<<a<<endl;
+        
+    }
+}
