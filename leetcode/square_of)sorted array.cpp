af229e4fc5ace5bacfcd8afa7b67d8fe1c409477
Vijay6923 <kumarviju2468@gmail.com>
2025-08-28 21:53:00 +0530

    leetcode

---
+++ leetcode/square_of)sorted array.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> sortedSquares(vector<int>& nums) {
+    vector<int> res(nums.size(), 0);
+    int left = 0;
+    int right = nums.size() - 1;
+
+    for (int i = nums.size() - 1; i >= 0; i--) {
+        if (abs(nums[left]) > abs(nums[right])) {
+            res[i] = nums[left] * nums[left];
+            left++;
+        } else {
+            res[i] = nums[right] * nums[right];
+            right--;
+        }
+    }
+    return res;
+}
+
+int main() {
+    vector<int> nums = {-7, -3, 2, 3, 11};
+    vector<int> ans = sortedSquares(nums);
+
+    for (int x : ans) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
