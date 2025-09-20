f9c06b0aa9b6cd03df6b1372817624a6157d3c45
Vijay Kumar <148934444+Vijay6923@users.noreply.github.com>
2025-09-20 19:51:30 +0530

    Create sort_the_sentence.cpp

---
+++ leetcode/sort_the_sentence.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string sortSentence(string s) {
+    vector<string> words(10, ""); 
+    for (int i = 0; i < s.size(); i++) { 
+        int j = i;
+        string word;
+        while (!isdigit(s[j])) { 
+            word.push_back(s[j]);
+            j++;
+        }
+        words[s[j] - '0'] = word;
+        i = j + 1; 
+    }
+
+    string answer; 
+    for (int i = 1; i < 10; i++) { 
+        if (words[i].length() > 0) {
+            answer += words[i];
+            answer.push_back(' '); 
+        }
+    }
+    answer.pop_back(); 
+    return answer; 
+}
+
+int main() {
+    string s;
+    cout << "Enter shuffled sentence: ";
+    getline(cin, s);
+
+    cout << "Sorted sentence: " << sortSentence(s) << endl;
+
+    return 0;
+}
