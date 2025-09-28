179228d6d131d5b158085730da7097a8ecc1bc24
Vijay Kumar <148934444+Vijay6923@users.noreply.github.com>
2025-09-28 21:23:28 +0530

    Create perimeter_triangle.cpp

---
+++ leetcode/perimeter_triangle.cpp
@@ -0,0 +1,10 @@
+int largestPerimeter(vector<int>& nums) {
+        int ans = 0, n = nums.size();
+        sort(begin(nums), end(nums));
+        for(int i = 1; i<n-1 ; i++){
+            int a = nums[i - 1], b = nums[i], c = nums[i + 1];
+            if(a + b > c)
+                ans = max(ans, (a + b + c));
+        }
+        return ans;    
+    }
