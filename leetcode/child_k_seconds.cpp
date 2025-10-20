1a8d8fd007c7be01a5fd5ac29ded7e8473fd42e0
Vijay Kumar <148934444+Vijay6923@users.noreply.github.com>
2025-10-20 14:29:59 +0530

    Refactor child counting logic in numberOfChild function

--- leetcode/child_k_seconds.cpp
+++ leetcode/child_k_seconds.cpp
@@ -3,21 +3,22 @@ using namespace std;
 
 int numberOfChild(int n, int k) {
     int ans = 0;
-    bool forward = true;
-    for (int i = 0; i < k; i++) {
-        if (forward) {
+    bool flag = true;
+    for(int i = 0; i < k; i++) {
+        if (flag) {
             ans++;
-            if (ans == n - 1) forward = false;
+            if (ans == n - 1) flag = false;
         } 
         else {
             ans--;
-            if (ans == 0) forward = true;
+            if (ans == 0) flag = true;
         }
     }
     return ans;
 }
 
 int main() {
+    // leetcode submission 
     int n, k;
     cout << "Enter number of children: ";
     cin >> n;
