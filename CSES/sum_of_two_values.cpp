9abe7aed32ff8509b1d2b32d6a06cf594db19b98
Vijay6923 <kumarviju2468@gmail.com>
2025-12-14 23:08:37 +0530

    sum of two values cses

--- CSES/sum_of_two_values.cpp
+++ CSES/sum_of_two_values.cpp
@@ -1,25 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     int n;
-    long long x;
+    int x;
     cin >> n >> x;
-    vector<long long> arr(n);
-    for(int i = 0; i < n; i++) {
+    vector< int> arr(n);
+    for( int  i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    unordered_map<long long, int>m; 
-
-    for(int i = 0; i < n; i++) {
-        long long rem = x - arr[i];
+    unordered_map< int, int >m; 
+    m.reserve(2*n);
+    m.max_load_factor(0.7);
+    for( int i = 0; i < n; i++) {
+        int  rem = x - arr[i];
         if(m.find(rem) != m.end()) {
             cout << m[rem] + 1 << " " << i + 1 << endl;
             return 0;
         }
         m[arr[i]] = i; 
     }
-
     cout << "IMPOSSIBLE" << endl;
-    return 0;
 }
